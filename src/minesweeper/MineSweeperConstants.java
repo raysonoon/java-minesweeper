c152381e9847b020bcab017715c91cc15905cffd
raysonoon <raysonoonhere@gmail.com>
2024-04-11 01:08:46 +0800

    Complete basic minesweeper in java

---
+++ src/minesweeper/MineSweeperConstants.java
@@ -0,0 +1,10 @@
+package minesweeper;
+/**
+ * Define the name-constants used in many classes of the game.
+ */
+public class MineSweeperConstants {
+   /** Number of rows */
+   public static final int ROWS = 10;
+   /** Number of columns */
+   public static final int COLS = 10;
+}
